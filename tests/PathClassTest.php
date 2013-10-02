f0a6ee976fd893162076d0036909dd936def57ed
Simon Dann <simon@photogabble.co.uk>
2013-10-02 13:20:24 +0100

    Added test assertion to cover path size method

--- tests/PathClassTest.php
+++ tests/PathClassTest.php
@@ -1,6 +1,7 @@
 <?php
 
-use \Carbontwelve\Tools\Formatters\Path;
+use Carbontwelve\Tools\Formatters\Path;
+use Icecave\Temptation\Temptation;
 
 class PathClassTest extends PHPUnit_Framework_TestCase {
 
@@ -23,5 +24,26 @@ class PathClassTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals( 'Hello', $this->pathClass->getPath() );
         $this->pathClass->setPath( 'World' );
         $this->assertEquals( 'World', $this->pathClass->getPath() );
+    }
+
+    public function testPathSizeForFile()
+    {
+        /** @var $temptation Icecave\Temptation\Temptation */
+        $temptation = new Temptation;
+        $file       = $temptation->createFile();
+        file_put_contents($file->path(), 'This is my temp file.');
+
+        $this->pathClass = new Path( $file->path() );
+        $this->assertEquals( $file->path(), $this->pathClass->getPath() );
+        $this->assertEquals( 21, $this->pathClass->size() );
+    }
+
+    public function testPathSizeForDir()
+    {
+        /** @var $temptation Icecave\Temptation\Temptation */
+        $temptation = new Temptation;
+
+        // Need to figure out how to use Temptation to emulate a directory structure filled with files
+
     }
 }
